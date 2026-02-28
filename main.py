295d6b09e0a2627eb4cd577fcfc16a37d311a889
23f3001101 <23f3001101@ds.study.iitm.ac.in>
2026-02-28 13:47:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 class CodeRequest(BaseModel):
